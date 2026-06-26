5c6c69b22f50f1ec939cfdf5aa72071ca8fc0ed4
Rani khatoon <ranikhutn856@gmail.com>
2026-06-26 10:55:59 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 console.log("API URL =", import.meta.env.VITE_API_URL);
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://interview-platform-9-hu11.onrender.com/api",
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
