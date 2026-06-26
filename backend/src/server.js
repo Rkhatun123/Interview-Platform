2b4177d72c54d6882ab3ef1464029bc93f02cc54
Rani khatoon <ranikhutn856@gmail.com>
2026-06-26 10:33:34 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin:"https://interview-platform-9-hu11.onrender.com/api",
+    origin:"https://interview-platform-10-65fu.onrender.com/api",
     credentials: true,
   })
 );
